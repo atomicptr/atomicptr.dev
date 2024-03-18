0e5bf3d5b232b762fc7efec7ff592879e30fbbca
Christopher Kaster <me@atomicptr.de>
2024-03-18 16:39:21 +0100

    fix links text showing despite no links being present

--- src/lib/components/PostLinksList.svelte
+++ src/lib/components/PostLinksList.svelte
@@ -7,17 +7,22 @@
     export let post: PostDetails;
 </script>
 
-{#if post.links}
+{#if post.links && post.links.length > 0}
     <div class="my-8">
-        <div class="text-2xl">
-            Links
-        </div>
+        <div class="text-2xl">Links</div>
 
         <div class="flex flex-col items-center w-full">
             {#each post.links as link}
                 <div class="my-2 w-full max-w-lg">
-                    <a class="btn btn-dark w-full h-auto p-4" href="{link.link}" target="_blank">
-                        <Fa icon={faLink} size="lg" />
+                    <a
+                        class="btn btn-dark w-full h-auto p-4"
+                        href={link.link}
+                        target="_blank"
+                    >
+                        <Fa
+                            icon={faLink}
+                            size="lg"
+                        />
                         <div class="flex flex-col text-left">
                             <span class="text-xl">{link.title}</span>
                             {#if link.description}
