c339508b768cb3d6a79f97bb434e881552065f45
Christopher Kaster <me@atomicptr.de>
2024-03-18 16:48:23 +0100

    improve rss feed by adding full posts to description, limit rendered rss posts to 10

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,5 @@ export default {
     blogTitle: "dev://atomicptr",
     description: "A blog primarily about programming",
     domainPrefix: "https://atomicptr.dev",
+    rssMaxNumberOfPosts: 10,
 };
--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,24 +1,41 @@
-import { findPosts, type Post } from "$lib/blog/nimbus";
+import { findPosts } from "$lib/blog/nimbus";
+import { parseBody } from "$lib/blog/parse";
 import config from "$lib/config";
 
 export const prerender = true;
 
+interface ParsedPost {
+    title: string;
+    slug: string;
+    content: string;
+    createdAt: Date;
+}
+
 export async function GET() {
     const posts = await findPosts();
+
+    const parsedPosts: ParsedPost[] = await Promise.all(
+        posts.slice(0, config.rssMaxNumberOfPosts).map(async ({ title, content, slug, created_at }) => ({
+            content: await parseBody(content),
+            createdAt: new Date(created_at),
+            slug,
+            title,
+        })),
+    );
 
     const headers = {
         "Cache-Control": "max-age=0, s-maxage=3600",
         "Content-Type": "application/xml",
     };
 
-    const body = rss(posts);
+    const body = rss(parsedPosts);
 
     return new Response(body, {
         headers,
     });
 }
 
-const rss = (posts: Post[]): string => `<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+const rss = (posts: ParsedPost[]): string => `<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
   <channel>
     <title>atomicptr.dev</title>
     <link>${config.domainPrefix}</link>
@@ -26,14 +43,15 @@ const rss = (posts: Post[]): string => `<rss xmlns:atom="http://www.w3.org/2005/
     <atom:link href="${config.domainPrefix}/rss.xml" rel="self" type="application/rss+xml" />
     ${posts
         .map(
-            post =>
-                `
+            post => `
         <item>
           <guid>${config.domainPrefix}/blog/${post.slug}/</guid>
           <title>${post.title}</title>
-          ${post.description ? `<description>${post.description}</description>` : ""}
+          <description>
+            <![CDATA[<a href='${config.domainPrefix}/blog/${post.slug}/'>Check out this post on my blog: ${config.blogTitle}</a><br/><br/><h1>${post.title}</h1>${post.content}]]>
+          </description>
           <link>${config.domainPrefix}/blog/${post.slug}/</link>
-          <pubDate>${new Date(post.created_at).toUTCString()}</pubDate>
+          <pubDate>${post.createdAt.toUTCString()}</pubDate>
         </item>
       `,
         )
