79d7f3cd6662fafec7a5c100d7d6ef6872e6a72a
Christopher Kaster <me@atomicptr.de>
2026-01-21 16:05:17 +0100

    import newest blog post, move more stuff to config, add giscus, improve styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,34 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+import {
+    transformerRenderWhitespace,
+    transformerRenderIndentGuides,
+    transformerNotationDiff,
+    transformerNotationFocus,
+    transformerMetaHighlight,
+} from "@shikijs/transformers";
 
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [mdx()],
+    markdown: {
+        shikiConfig: {
+            theme: "catppuccin-mocha",
+            transformers: [
+                transformerRenderWhitespace({ position: "all" }),
+                // transformerRenderIndentGuides(),
+                transformerNotationDiff(),
+                transformerNotationFocus(),
+                transformerMetaHighlight(),
+            ],
+        },
+    },
+
+    integrations: [mdx(), icon()],
 });
+
---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export default {
+    title: "dev://atomicptr",
+
+    author: {
+        name: "atomicptr",
+        email: "me@atomicptr.de",
+    },
+
+    urls: {
+        github: "https://github.com/atomicptr",
+        support: "https://buymeacoffee.com/atomicptr",
+        xcom: "https://x.com/atomicptr",
+    },
+
+    giscus: {
+        repo: "atomicptr/atomicptr.dev",
+        repoId: "R_kgDOLCEAgg",
+        category: "Comments",
+        categoryId: "DIC_kwDOLCEAgs4CcRWF",
+    },
+};
